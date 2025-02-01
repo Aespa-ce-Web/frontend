695fa586df62f21f7cb84c031f028d5a6f8c5ad5
William Boyard <williamboyard1@gmail.com>
2025-02-01 22:14:03 +0100

    add search ressources

--- src/app/ressources/ressources.component.ts
+++ src/app/ressources/ressources.component.ts
@@ -2,16 +2,19 @@ import { Component } from '@angular/core';
 import { Ressource } from '../models/Ressource';
 import { RessourcesService } from '../services/ressources/ressources.service';
 import { RessourceCardComponent } from './ressource-card/ressource-card.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ressources',
   standalone: true,
-  imports: [RessourceCardComponent],
+  imports: [RessourceCardComponent,FormsModule,CommonModule],
   templateUrl: './ressources.component.html',
   styleUrl: './ressources.component.css'
 })
 export class RessourcesComponent {
   ressources: Ressource[] = [];
+  searchTerm: string = '';
   
     constructor(private ressourcesService: RessourcesService) {}
   
@@ -25,4 +28,14 @@ export class RessourcesComponent {
         }
       });
     }
+
+      get filteredRessources(): Ressource[] {
+        if (!this.searchTerm) {
+          return this.ressources;
+        }
+        const term = this.searchTerm.toLowerCase();
+        return this.ressources.filter(ressources =>
+          ressources.nom.toLowerCase().includes(term) || ressources.id.toString().includes(term)
+        );
+      }
 }
